987a22519fd995ff93675084a17be93d6de01055
Alexey <4681325+qqrm@users.noreply.github.com>
2025-07-13 20:43:15 +0300

    Fix snapshot path for wasm

--- tests/geometry.rs
+++ tests/geometry.rs
@@ -59,10 +59,7 @@ fn candle_geometry_snapshot() {
         );
     }
 
-    unsafe {
-        std::env::set_var("INSTA_WORKSPACE_ROOT", env!("CARGO_MANIFEST_DIR"));
-    }
-    with_settings!({snapshot_path => "tests/fixtures"}, {
+    with_settings!({snapshot_path => concat!(env!("CARGO_MANIFEST_DIR"), "/tests/fixtures")}, {
         assert_json_snapshot!("candle_vertices", result);
     });
 }
